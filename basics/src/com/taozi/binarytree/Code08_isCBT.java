37ab09ccb3ef63adc41cb41eeb83e6f480cc4949
zhaotaosheng <zts-0616@163.com>
2020-12-15 22:33:54 +0800

    isCBT isBCT

---
+++ basics/src/com/taozi/binarytree/Code08_isCBT.java
@@ -0,0 +1,54 @@
+package com.taozi.binarytree;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * 判断二叉树是否是完全二叉树
+ *
+ * @author ZhaoTaoSheng
+ * @since 2020/12/15 20:41
+ */
+public class Code08_isCBT {
+    // 二叉树节点
+    private static class Node {
+        public Integer value;
+        public Node left;
+        public Node right;
+    }
+
+    /**
+     * 用宽度遍历所有节点，根据节点孩子情况判断是否为完全二叉树
+     * 完全二叉树的特征：每一层的节点都是从左往右排列，当前层不满不会排下一层
+     *
+     * @param root 树根节点
+     * @return 是否为完全二叉树
+     */
+    public boolean isCBT(Node root) {
+        if (root == null) return true;
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(root);
+        // 是否遇到孩子不双全的节点，如果是的话后续的节点都应该是叶子节点
+        boolean leaf = false;
+        // 左子节点
+        Node left = null;
+        // 右子节点
+        Node right = null;
+        // 当前处理节点
+        Node cur = root;
+        while (!queue.isEmpty()) {
+            cur = queue.poll();
+            left = cur.left;
+            right = cur.right;
+            // 如果为叶子节点且有孩子，说明不是完全二叉树
+            if (leaf && (left != null || right != null)) return false;
+            // 如果有右孩子但没有左孩子，说明不是完全二叉树
+            if (left == null && right != null) return false;
+            // 如果当前节点左右孩子不双全，说明后续都应该是叶子节点
+            if (left == null || right == null) leaf = true;
+            if (left != null) queue.add(left);
+            if (right != null) queue.add(right);
+        }
+        return true;
+    }
+}
---
+++ basics/src/com/taozi/binarytree/Code09_isBST.java
@@ -0,0 +1,85 @@
+package com.taozi.binarytree;
+
+/**
+ * 判断二叉树是否是搜索二叉树
+ *
+ * @author ZhaoTaoSheng
+ * @since 2020/12/15 22:04
+ */
+public class Code09_isBST {
+    // 二叉树节点
+    private static class Node {
+        public int value;
+        public Node left;
+        public Node right;
+    }
+
+    /**
+     * 通过二叉树递归来判断，还有一种方法是根据搜索二叉树的中序遍历有序
+     *
+     * @param root 二叉树根节点
+     * @return 是否为搜索二叉树
+     */
+    public boolean isBST(Node root) {
+        if (root == null) return true;
+        return process(root).isBST;
+    }
+
+    // 需要子树的信息
+    private static class Info {
+        public boolean isBST;
+        public int min;
+        public int max;
+
+        public Info(boolean isBST, int min, int max) {
+            this.isBST = isBST;
+            this.min = min;
+            this.max = max;
+        }
+    }
+
+    /**
+     * 当前树是搜索二叉树的条件：
+     * 1.左子树和右子树都是搜索二叉树
+     * 2.左子树最大值 < 当前值 < 右子树最小值
+     * <p>
+     * 所以我需要知道我左子树和右子树的信息，将子节点返回信息组合为自己的信息在返回给父节点
+     *
+     * @param head 当前树头节点
+     * @return 信息
+     */
+    public Info process(Node head) {
+        // 如果当前树为空，则没有信息返回
+        if (head == null) return null;
+        // 获取左子树信息
+        Info leftInfo = process(head.left);
+        // 获取右子树信息
+        Info rightInfo = process(head.right);
+        // 找到当前树的最小值和最大值
+        // 该值在当前、左子树、右子树中寻找
+        int min = head.value;
+        int max = head.value;
+        if (leftInfo != null) {
+            min = Math.min(min, leftInfo.min);
+            max = Math.max(max, leftInfo.max);
+        }
+        if (rightInfo != null) {
+            min = Math.min(min, rightInfo.min);
+            max = Math.max(max, rightInfo.max);
+        }
+        // 判断当前树是否为平衡二叉树
+        boolean isBST = true;
+        // 如果左子树或者右子树不为平衡二叉树，那当前树不为平衡二叉树
+        if ((leftInfo != null && !leftInfo.isBST)
+                || (rightInfo != null && !rightInfo.isBST)) {
+            isBST = false;
+        }
+        // 如果当前值 <= 左子树最大值 或者 右子树最小值 <= 当前值，那当前树不为平衡二叉树
+        if ((leftInfo != null && leftInfo.max >= head.value)
+                || (rightInfo != null && rightInfo.min <= head.value)) {
+            isBST = false;
+        }
+        // 将自己的信息返回给父节点
+        return new Info(isBST, min, max);
+    }
+}
