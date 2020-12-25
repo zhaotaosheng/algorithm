c4eaaa67c985249fb0ed66ee77e6be331afb500c
zhaotaosheng <zts-0616@163.com>
2020-12-25 09:55:24 +0800

    number of islands 2

--- basics/src/com/taozi/unionfind/Code04_NumberOfIslandsII.java
+++ basics/src/com/taozi/unionfind/Code04_NumberOfIslandsII.java
@@ -5,11 +5,21 @@ import java.util.Arrays;
 import java.util.List;
 
 /**
+ * 初始网格都为0，动态给出陆地位置1，判断实时岛屿数量
+ *
  * @author ZhaoTaoSheng
  * @since 2020/12/24 21:25
  */
 public class Code04_NumberOfIslandsII {
 
+    /**
+     * 使用并查集的方式合并陆地
+     *
+     * @param m         网格
+     * @param n         网格
+     * @param positions 实时给出的陆地位置
+     * @return 实时的岛屿数量
+     */
     public List<Integer> numIslands21(int m, int n, int[][] positions) {
         UnionFind unionFind = new UnionFind(m, n);
         List<Integer> res = new ArrayList<>();
@@ -19,6 +29,7 @@ public class Code04_NumberOfIslandsII {
         return res;
     }
 
+    // 基于数组实现的并查集
     private static class UnionFind {
         public int[] parents;
         public int[] sizes;
@@ -32,33 +43,55 @@ public class Code04_NumberOfIslandsII {
             col = n;
             sets = 0;
             int size = row * col;
+            // 初始化都是水，所以都为0
             parents = new int[size];
             sizes = new int[size];
             help = new int[size];
         }
 
+        /**
+         * 每次空降一个陆地试，将该位置与其上下左右的陆地合并
+         *
+         * @param r 空降纵坐标
+         * @param c 空降横坐标
+         * @return 当前岛屿数量
+         */
         public Integer add(int r, int c) {
             int index = r * col + c;
+            // 如果当前位置的数量不为0，说明之前空降过该点，不处理
             if (sizes[index] == 0) {
+                // 初始化该位置
                 parents[index] = index;
                 sizes[index] = 1;
                 sets++;
+                // 尝试与其上下左右的陆地合并
                 union(r - 1, c, r, c);
                 union(r + 1, c, r, c);
                 union(r, c - 1, r, c);
                 union(r, c + 1, r, c);
             }
+            // 返回岛屿数量
             return sets;
         }
 
+        /**
+         * 陆地合并
+         *
+         * @param r1 第一块陆地的纵坐标
+         * @param c1 第一块陆地的横坐标
+         * @param r2 第二块陆地的纵坐标
+         * @param c2 第一块陆地的横坐标
+         */
         private void union(int r1, int c1, int r2, int c2) {
             if (r1 < 0 || r1 == row || r2 < 0 || r2 == row
                     || c1 < 0 || c1 == col || c2 < 0 || c2 == col) return;
             int i = r1 * col + c1;
             int j = r2 * col + c2;
+            // 如果这两块陆地中有一个没空降过陆地，那么直接返回
             if (sizes[i] == 0 || sizes[j] == 0) return;
             int root1 = findRoot(i);
             int root2 = findRoot(j);
+            // 如果两块陆地不属于一个岛屿那么将其合并，并将岛屿数量减1
             if (root1 != root2) {
                 if (sizes[root1] > sizes[root2]) {
                     sizes[root1] += sizes[root2];
@@ -71,165 +104,22 @@ public class Code04_NumberOfIslandsII {
             }
         }
 
+        /**
+         * 找到当前陆地所在岛屿的代表节点
+         *
+         * @param index 当前陆地坐标
+         * @return 代表节点坐标
+         */
         private int findRoot(int index) {
             int path = 0;
             while (index != parents[index]) {
                 index = parents[index];
                 help[path++] = index;
             }
-//            while (path != 0) {
-//                parents[help[--path]] = index;
-//            }
-            for (path--; path >= 0; path--) {
-                parents[help[path]] = index;
+            while (path != 0) {
+                parents[help[--path]] = index;
             }
             return index;
         }
-    }
-
-    public List<Integer> numIslands22(int m, int n, int[][] positions) {
-        int[] arr = new int[m * n];
-        Arrays.fill(arr, 0);
-        List<Integer> res = new ArrayList<>();
-        int num = 0;
-        for (int[] position : positions) {
-            num++;
-            int x = position[0];
-            int y = position[1];
-            if (hasLandRound(arr, x, y, m, n)) {
-                num--;
-            }
-            arr[x * n + y] = 1;
-            res.add(num);
-        }
-        return res;
-    }
-
-    public boolean hasLandRound1(int[] arr, int x, int y, int m, int n) {
-        boolean hasLandRound = false;
-        int index;
-        if ((index = x * n + y + 1) < m * n && arr[index] == 1) hasLandRound = true;
-        else if (0 < (index = x * n + y - 1) && index < m * n && arr[index] == 1) hasLandRound = true;
-        else if ((index = (x + 1) * n + y) < m * n && arr[index] == 1) hasLandRound = true;
-        else if (0 < (index = (x - 1) * n + y) && index < m * n && arr[index] == 1) hasLandRound = true;
-        return hasLandRound;
-    }
-
-    public boolean hasLandRound(int[] arr, int x, int y, int m, int n) {
-        if (arr[x * n + y] == 1) return false;
-        boolean hasLandRound = false;
-        if (x * n + y + 1 < m * n && arr[x * n + y + 1] == 1) hasLandRound = true;
-        else if (0 < x * n + y - 1 && x * n + y - 1 < m * n && arr[x * n + y - 1] == 1) hasLandRound = true;
-        else if ((x + 1) * n + y < m * n && arr[(x + 1) * n + y] == 1) hasLandRound = true;
-        else if (0 < (x - 1) * n + y && (x - 1) * n + y < m * n && arr[(x - 1) * n + y] == 1) hasLandRound = true;
-        return hasLandRound;
-    }
-
-    public static void main(String[] args) {
-        Code04_NumberOfIslandsII code04_numberOfIslandsII = new Code04_NumberOfIslandsII();
-        int m = 3;
-        int n = 3;
-        for (int j = 0; j < 1; j++) {
-            int[][] positions = new int[(int) (Math.random() * m * n)][2];
-            for (int i = 0; i < positions.length; i++) {
-                positions[i] = new int[]{(int) (Math.random() * m), (int) (Math.random() * n)};
-            }
-            List<Integer> integers1 = code04_numberOfIslandsII.numIslands21(m, n, positions);
-            List<Integer> integers2 = code04_numberOfIslandsII.numIslands22(m, n, positions);
-            for (int i = 0; i < integers1.size(); i++) {
-                if (!integers1.get(i).equals(integers2.get(i))) {
-                    for (int[] position : positions) {
-                        System.out.println(position[0] + " , " + position[1]);
-                    }
-                    System.out.println(integers1.get(i));
-                    System.out.println(integers2.get(i));
-                    System.out.println("错误！！！！！！！！！！！！！");
-                    return;
-                }
-            }
-        }
-        System.out.println("完成！！！！！！！！！！");
-    }
-
-    public static List<Integer> numIslands23(int m, int n, int[][] positions) {
-        UnionFind1 uf = new UnionFind1(m, n);
-        List<Integer> ans = new ArrayList<>();
-        for (int[] position : positions) {
-            ans.add(uf.connect(position[0], position[1]));
-        }
-        return ans;
-    }
-
-    public static class UnionFind1 {
-        private int[] parent;
-        private int[] size;
-        private int[] help;
-        private final int row;
-        private final int col;
-        private int sets;
-
-        public UnionFind1(int m, int n) {
-            row = m;
-            col = n;
-            sets = 0;
-            int len = row * col;
-            parent = new int[len];
-            size = new int[len];
-            help = new int[len];
-        }
-
-        private int index(int r, int c) {
-            return r * col + c;
-        }
-
-        private int find(int i) {
-            int hi = 0;
-            while (i != parent[i]) {
-                help[hi++] = i;
-                i = parent[i];
-            }
-            for (hi--; hi >= 0; hi--) {
-                parent[help[hi]] = i;
-            }
-            return i;
-        }
-
-        private void union(int r1, int c1, int r2, int c2) {
-            if (r1 < 0 || r1 == row || r2 < 0 || r2 == row || c1 < 0 || c1 == col || c2 < 0 || c2 == col) {
-                return;
-            }
-            int i1 = index(r1, c1);
-            int i2 = index(r2, c2);
-            if (size[i1] == 0 || size[i2] == 0) {
-                return;
-            }
-            int f1 = find(i1);
-            int f2 = find(i2);
-            if (f1 != f2) {
-                if (size[f1] >= size[f2]) {
-                    size[f1] += size[f2];
-                    parent[f2] = f1;
-                } else {
-                    size[f2] += size[f1];
-                    parent[f1] = f2;
-                }
-                sets--;
-            }
-        }
-
-        public int connect(int r, int c) {
-            int index = index(r, c);
-            if (size[index] == 0) {
-                parent[index] = index;
-                size[index] = 1;
-                sets++;
-                union(r - 1, c, r, c);
-                union(r + 1, c, r, c);
-                union(r, c - 1, r, c);
-                union(r, c + 1, r, c);
-            }
-            return sets;
-        }
-
     }
 }
