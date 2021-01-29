faaa6433aa971efacad55a039b3f4fd2f177c3ba
zhaotaosheng <zts-0616@163.com>
2021-01-29 13:28:28 +0800

    monotonous stack

---
+++ subject/src/com/taozi/monotonousstack/Code04_MaximalRectangleInHistogram.java
@@ -0,0 +1,17 @@
+package com.taozi.monotonousstack;
+
+/**
+ * 给定一个二维数组matrix，其中的值不是0就是1，
+ * 返回全部由1组成的最大子矩形，内部有多少个1
+ *
+ * @author taosheng.zhao
+ * @since 2021/1/29 12:38
+ */
+public class Code04_MaximalRectangleInHistogram {
+
+    public static int maximalNumber(int[][] matrix) {
+        int res = 0;
+
+        return res;
+    }
+}
